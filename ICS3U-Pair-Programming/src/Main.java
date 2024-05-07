8a8a0713d0cf26e1e3ee820680f40acc9eaf6534
Geoffrey <159826017+Geoffrey88@users.noreply.github.com>
2024-05-07 13:56:11 -0400

    made random object

--- ICS3U-Pair-Programming/src/Main.java
+++ ICS3U-Pair-Programming/src/Main.java
@@ -1,6 +1,20 @@
 import javax.swing.JOptionPane;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         JOptionPane.showInputDialog("Test");
+
+    Random random = new Random();
+    
+    int answer = 0; 
+    int guess = 0;
+
+    while (guess == answer){
+    
+        answer = random.nextInt(9);
+        System.out.println(answer);
+    
     }
+    
+}
 }
