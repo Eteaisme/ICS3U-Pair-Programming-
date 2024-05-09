14494c1f38c9f74583e31504362955f3609332d7
Eteaisme <eteaisme@gmail.com>
2024-05-09 13:56:35 -0400

    Expanded and fixed geoffrey's logic

---
+++ ICS3U-Pair-Programming/src/Game.java
@@ -0,0 +1,8 @@
+public class Game{
+    public String answer;
+    public String answer2;
+    public String guess;
+    public int round = 1; 
+
+
+};
--- ICS3U-Pair-Programming/src/Main.java
+++ ICS3U-Pair-Programming/src/Main.java
@@ -1,34 +1,49 @@
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        JOptionPane.showInputDialog("Test");
+    /*
+    Creating Objects
+    */    
+    Scanner Scan = new Scanner(System.in);
+    Random random = new Random(); 
+    Game gameObject = new Game();
 
-    Random random = new Random();
-    
-    string answer = 0; 
-    int guess = 0;
-    int round = 1;
-    string answer2 = 1;
-    answer = random.nextInt(9);
-        System.out.println(answer);
-    while (guess == answer);{
-    
-       
-    for (round){ 
-        answer2 = random.nextInt(9);
-        answer = answer + answer2;
-        System.out.println(answer);
+    /*Setting Answer to something random */
+    gameObject.answer = Integer.toString(random.nextInt(9));
+    System.out.println(gameObject.answer);
 
+    /*First Guess */
+    System.out.print("Guess the number: ");
+    gameObject.guess = Scan.nextLine();
+
+
+
+/*While loop */
+ while (gameObject.guess == gameObject.answer);{
+    //While loop notification 
+    System.out.println("Your in the while loop");
+
+    //For loop
+        for(int i = 0; i<gameObject.round; i++){
+
+        //Create answer 2 and concatenate it 
+        gameObject.answer2 = Integer.toString(random.nextInt(9));
+        gameObject.answer = gameObject.answer + gameObject.answer2;
+            }       
+
+            //Print answer and guess
+            System.out.println(gameObject.answer);
+            System.out.print("Guees the number: ");
+            gameObject.guess = Scan.nextLine();
+            
+            gameObject.round ++;
+
+            if (gameObject.guess == gameObject.answer); 
+            gameObject.guess = "";
+            else
+        }   
     }
-
-
-    }
-    
 }
-}
-
-
-public answers(int answer, int answer2){
-            this.answer = 0;
-            this.round = 1; 
