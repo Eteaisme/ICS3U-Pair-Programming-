3148cd61d1ca7f4cec86b55a2060d3d0c984a7d8
Elliot Tam <eteaisme@gmail.com>
2024-05-13 11:48:11 -0400

    Added GUI

--- ICS3U-Pair-Programming/src/Game.java
+++ ICS3U-Pair-Programming/src/Game.java
@@ -2,7 +2,11 @@ public class Game{
     public String answer;
     public String answer2;
     public String guess;
+    public String gameState = "Y"; 
+    public int time = 5; 
     public int round = 1; 
+
+
 
 
 };
--- ICS3U-Pair-Programming/src/Main.java
+++ ICS3U-Pair-Programming/src/Main.java
@@ -1,49 +1,43 @@
-import java.util.Scanner;
 import javax.swing.JOptionPane;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-    /*
-    Creating Objects
-    */    
-    Scanner Scan = new Scanner(System.in);
-    Random random = new Random(); 
-    Game gameObject = new Game();
+        /*
+        Creating Objects
+        */
+        Random random = new Random();
+        Game gameObject = new Game();
 
-    /*Setting Answer to something random */
-    gameObject.answer = Integer.toString(random.nextInt(9));
-    System.out.println(gameObject.answer);
+        while (gameObject.gameState == "Y") {
 
-    /*First Guess */
-    System.out.print("Guess the number: ");
-    gameObject.guess = Scan.nextLine();
-
-
-
-/*While loop */
- while (gameObject.guess == gameObject.answer);{
-    //While loop notification 
-    System.out.println("Your in the while loop");
-
-    //For loop
-        for(int i = 0; i<gameObject.round; i++){
-
-        //Create answer 2 and concatenate it 
-        gameObject.answer2 = Integer.toString(random.nextInt(9));
-        gameObject.answer = gameObject.answer + gameObject.answer2;
-            }       
-
-            //Print answer and guess
+            /*Setting Answer to something random */
+            gameObject.answer = Integer.toString(random.nextInt(9));
             System.out.println(gameObject.answer);
-            System.out.print("Guees the number: ");
-            gameObject.guess = Scan.nextLine();
-            
-            gameObject.round ++;
 
-            if (gameObject.guess == gameObject.answer); 
-            gameObject.guess = "";
-            else
-        }   
+            /*First Guess */
+            System.out.print("Type Back The Number: ");
+            gameObject.guess = JOptionPane.showInputDialog(gameObject.answer,null);
+
+            /*While loop */
+            while (Integer.parseInt(gameObject.guess) == Integer.parseInt(gameObject.answer)) {
+
+                //For loop
+                for (int i = 0; i < gameObject.round; i++) {
+
+                    //Create answer 2 and concatenate it 
+                    gameObject.answer2 = Integer.toString(random.nextInt(9));
+                    gameObject.answer = gameObject.answer + gameObject.answer2;
+                }
+
+                //Print answer and guess
+                System.out.println(gameObject.answer);
+                gameObject.guess = JOptionPane.showInputDialog(gameObject.answer ,null);
+
+            }
+            gameObject.gameState = JOptionPane.showInputDialog("Sorry wrong answer, would you like to play again? [Y/N]",null); 
+            System.out.println(gameObject.gameState);
+        }
+
     }
 }
